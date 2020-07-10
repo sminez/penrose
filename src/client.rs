30c5222e551abe963dc10131cab5317d77f1177a
IDAM <innes.andersonmorrison@gmail.com>
2020-07-10 13:07:03 +0100

    removing unused methods on Client struct

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,5 @@
 //! Metadata around X clients and manipulating them
-use crate::data_types::{Border, ColorScheme, WinId};
-use crate::xconnection::XConn;
+use crate::data_types::WinId;
 
 /**
  * Meta-data around a client window that we are handling.
@@ -39,24 +38,5 @@ impl Client {
 
     pub fn class(&self) -> &str {
         &self.wm_class
-    }
-
-    pub fn focus(&mut self, conn: &dyn XConn, scheme: &ColorScheme) {
-        conn.focus_client(self.id);
-        self.set_window_border(conn, Border::Focused, scheme);
-    }
-
-    pub fn unfocus(&mut self, conn: &dyn XConn, scheme: &ColorScheme) {
-        self.set_window_border(conn, Border::Unfocused, scheme);
-    }
-
-    fn set_window_border(&mut self, conn: &dyn XConn, border: Border, scheme: &ColorScheme) {
-        let color = match border {
-            Border::Urgent => scheme.urgent,
-            Border::Focused => scheme.highlight,
-            Border::Unfocused => scheme.fg_1,
-        };
-
-        conn.set_client_border_color(self.id, color);
     }
 }
