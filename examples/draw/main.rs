ada6b4df103249e6f2b2fcdd5aefaa2cd2294922
M@ Dunlap <matthew.dunlap@gmail.com>
2020-10-08 15:01:48 -0600

    removed extraneous intos to placate clippy

--- examples/draw/main.rs
+++ examples/draw/main.rs
@@ -68,20 +68,20 @@ fn simple_draw() -> Result<()> {
 
     let mut ctx = drw.context_for(id)?;
 
-    ctx.color(&WHITE.into());
+    ctx.color(&WHITE);
     ctx.rectangle(0.0, 0.0, w as f64, HEIGHT as f64);
     ctx.translate(1.0, 1.0);
 
-    ctx.color(&BLACK.into());
+    ctx.color(&BLACK);
     ctx.font(PROFONT, 12)?;
     let (offset, _) = ctx.text("this is a simple test", 0.0, (0.0, 8.0))?;
 
-    ctx.color(&RED.into());
+    ctx.color(&RED);
     ctx.font(SERIF, 10)?;
     ctx.translate((offset + 5.0) as f64, 0.0);
     let (offset, _) = ctx.text("BORK BORK!", 0.0, (0.0, 0.0))?;
 
-    ctx.color(&PURPLE.into());
+    ctx.color(&PURPLE);
     ctx.font(FIRA, 10)?;
     ctx.translate((offset + 5.0) as f64, 0.0);
     ctx.text("Look at all the colors!", 0.0, (0.0, 0.0))?;
