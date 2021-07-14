635c56d1241bbe60fb23d9da9da2274f7a50ea06
Daan Vanoverloop <daan@vanoverloop.xyz>
2021-07-14 13:23:24 +0200

    Dwindle layout

--- src/contrib/layouts.rs
+++ src/contrib/layouts.rs
@@ -51,3 +51,40 @@ pub fn paper(
         })
         .collect()
 }
+
+fn dwindle_recurisive(clients: &[&Client], region: &Region, horizontal: bool) -> Vec<ResizeAction> {
+    if clients.len() > 1 {
+        let split = ((if horizontal { region.w } else { region.h } as f32) / 2.) as u32;
+        let (main, other) = if horizontal {
+            region.split_at_width(split)
+        } else {
+            region.split_at_height(split)
+        }
+        .unwrap();
+
+        let mut vec = dwindle_recurisive(&clients[..clients.len() - 1], &other, !horizontal);
+        vec.push((clients.last().unwrap().id(), Some(main)));
+        vec
+    } else {
+        clients
+            .get(0)
+            .map(|c| vec![(c.id(), Some(*region))])
+            .unwrap_or(Vec::new())
+    }
+}
+
+/**
+ * A layout based on the dwindle layout from AwesomeWM.
+ *
+ * The second region is recursively split in two other regions, alternating between
+ * splitting horizontally and vertically.
+ */
+pub fn dwindle(
+    clients: &[&Client],
+    _: Option<Xid>,
+    monitor_region: &Region,
+    _: u32,
+    _: f32,
+) -> Vec<ResizeAction> {
+    dwindle_recurisive(clients, monitor_region, true)
+}
