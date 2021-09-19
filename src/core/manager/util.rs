a2856449c4c763fb6f10b666efd314af5263f743
Benjamin Davies <bentendavies@gmail.com>
2021-09-19 19:14:19 +1200

    Fix underflow when resizing Regions (#186)
    * Fix underflow
    
    * Add warnings for potential underflows
    
    * Add pad_region_tiny test
    
    * Add tests for position_floating_client
    
    * Prefix unused param with `_`

--- src/core/manager/util.rs
+++ src/core/manager/util.rs
@@ -16,6 +16,14 @@ pub(super) fn pad_region(region: &Region, gapless: bool, gap_px: u32, border_px:
     let gpx = if gapless { 0 } else { gap_px };
     let padding = 2 * (border_px + gpx);
     let (x, y, w, h) = region.values();
+
+    // Check that the resulting size would not be zero or negative
+    // Do not allow zero-size as this is chosen by the WM
+    if w <= padding || h <= padding {
+        warn!("not padding region to avoid integer underflow");
+        return *region;
+    }
+
     Region::new(x + gpx, y + gpx, w - padding, h - padding)
 }
 
@@ -33,12 +41,20 @@ where
     let (sx, sy, _, _) = screen_region.values();
     x = if x < sx { sx } else { x };
     y = if y < sy { sy } else { y };
-    let reg = Region::new(
-        x + border_px,
-        y + border_px,
-        w - (2 * border_px),
-        h - (2 * border_px),
-    );
+
+    // Check that the resulting size would not be negative
+    // Allow zero-size here as it is chosen by the client
+    let reg = if w >= 2 * border_px && h >= 2 * border_px {
+        Region::new(
+            x + border_px,
+            y + border_px,
+            w - (2 * border_px),
+            h - (2 * border_px),
+        )
+    } else {
+        warn!("floating client too small {}", id);
+        Region::new(x, y, w, h)
+    };
 
     Ok(conn.position_client(id, reg, border_px, false)?)
 }
@@ -89,6 +105,7 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::__test_helpers::*;
 
     #[test]
     fn pad_region_centered() {
@@ -97,5 +114,42 @@ mod tests {
         let b = 3;
         assert_eq!(pad_region(&r, false, g, b), Region::new(10, 10, 174, 74));
         assert_eq!(pad_region(&r, true, g, b), Region::new(0, 0, 194, 94));
+    }
+
+    #[test]
+    fn pad_region_tiny() {
+        let r = Region::new(0, 0, 3, 3);
+        let g = 10;
+        let b = 3;
+        assert_eq!(pad_region(&r, false, g, b), r);
+        assert_eq!(pad_region(&r, true, g, b), r);
+    }
+
+    #[test]
+    fn position_floating() {
+        let conn = TestXConn::new(1, vec![], vec![]);
+        conn.position_client(0, Region::new(0, 0, 400, 300), 2, false)
+            .unwrap();
+
+        position_floating_client(&conn, 0, Region::default(), 2).unwrap();
+
+        assert_eq!(
+            conn.client_geometry(0).unwrap(),
+            Region::new(2, 2, 396, 296)
+        );
+    }
+
+    #[test]
+    fn position_floating_tiny() {
+        let conn = TestXConn::new(1, vec![], vec![]);
+        conn.position_client(0, Region::new(0, 0, 4, 3), 2, false)
+            .unwrap();
+
+        position_floating_client(&conn, 0, Region::default(), 2).unwrap();
+
+        assert_eq!(
+            conn.client_geometry(0).unwrap(),
+            Region::new(0, 0, 4, 3)
+        );
     }
 }
