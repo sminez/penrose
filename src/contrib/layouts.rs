5efd2bcfa3831d02b87056583e66eac8bb92cb9d
Daan Vanoverloop <daan@vanoverloop.xyz>
2021-09-19 10:18:39 +0200

    Dwindle layout (#180)
    * Dwindle layout
    
    * Stop recursing in dwindle layout when region too small

--- src/contrib/layouts.rs
+++ src/contrib/layouts.rs
@@ -51,3 +51,60 @@ pub fn paper(
         })
         .collect()
 }
+
+fn dwindle_recurisive(
+    clients: &[&Client],
+    region: &Region,
+    horizontal: bool,
+    min_size: u32,
+) -> Vec<ResizeAction> {
+    if clients.len() > 1 {
+        if region.w < min_size || region.h < min_size {
+            clients
+                .iter()
+                .enumerate()
+                .map(|(i, c)| {
+                    if i == 0 {
+                        (c.id(), Some(*region))
+                    } else {
+                        (c.id(), None)
+                    }
+                })
+                .collect()
+        } else {
+            let split = ((if horizontal { region.w } else { region.h } as f32) / 2.) as u32;
+            let (main, other) = if horizontal {
+                region.split_at_width(split)
+            } else {
+                region.split_at_height(split)
+            }
+            .unwrap();
+
+            let mut vec =
+                dwindle_recurisive(&clients[..clients.len() - 1], &other, !horizontal, min_size);
+            vec.push((clients.last().unwrap().id(), Some(main)));
+            vec
+        }
+    } else {
+        clients
+            .get(0)
+            .map(|c| vec![(c.id(), Some(*region))])
+            .unwrap_or(Vec::new())
+    }
+}
+
+/**
+ * A layout based on the dwindle layout from AwesomeWM.
+ *
+ * The second region is recursively split in two other regions, alternating between
+ * splitting horizontally and vertically.
+ */
+pub fn dwindle(
+    clients: &[&Client],
+    _: Option<Xid>,
+    monitor_region: &Region,
+    _: u32,
+    _: f32,
+) -> Vec<ResizeAction> {
+    dwindle_recurisive(clients, monitor_region, true, 50)
+}
