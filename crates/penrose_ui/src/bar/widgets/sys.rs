37d0fbdcff39eed2a7f7bcff7c16bb5013cae26c
Innes Anderson-Morrison <innes.andersonmorrison@gmail.com>
2022-11-07 09:08:10 +0000

    updating sys widgets

--- crates/penrose_ui/src/bar/widgets/sys.rs
+++ crates/penrose_ui/src/bar/widgets/sys.rs
@@ -8,10 +8,10 @@ use std::fs;
 /// If the given battery name is not found on this system, this widget will
 /// render as an empty string.
 pub fn battery_summary(bat: &'static str, style: &TextStyle) -> RefreshText {
-    RefreshText::new(style, move || get_battery_text(bat).unwrap_or_default())
+    RefreshText::new(style, move || battery_text(bat).unwrap_or_default())
 }
 
-fn get_battery_text(bat: &str) -> Option<String> {
+fn battery_text(bat: &str) -> Option<String> {
     let status = read_sys_file(bat, "status")?;
     let energy_now: u32 = read_sys_file(bat, "energy_now")?.parse().ok()?;
     let energy_full: u32 = read_sys_file(bat, "energy_full")?.parse().ok()?;
@@ -56,19 +56,19 @@ pub fn current_date_and_time(style: &TextStyle) -> RefreshText {
 /// Display the ESSID currently connected to and the signal quality as
 /// a percentage.
 pub fn wifi_network(style: &TextStyle) -> RefreshText {
-    RefreshText::new(style, move || get_wifi_text().unwrap_or_default())
+    RefreshText::new(style, move || wifi_text().unwrap_or_default())
 }
 
-fn get_wifi_text() -> Option<String> {
-    let (interface, essid) = get_interface_and_essid()?;
-    let signal = get_signal_quality(&interface)?;
+fn wifi_text() -> Option<String> {
+    let (interface, essid) = interface_and_essid()?;
+    let signal = signal_quality(&interface)?;
 
-    Some(format!("{essid} {signal}%"))
+    Some(format!("<{essid} {signal}%>"))
 }
 
 // Read the interface name and essid via iwgetid.
 //   Output format is '$interface    ESSID:"$essid"'
-fn get_interface_and_essid() -> Option<(String, String)> {
+fn interface_and_essid() -> Option<(String, String)> {
     let raw = spawn_for_output("iwgetid").ok()?;
     let mut iter = raw.split(':');
 
@@ -80,7 +80,7 @@ fn get_interface_and_essid() -> Option<(String, String)> {
 }
 
 // Parsing the format described here: https://hewlettpackard.github.io/wireless-tools/Linux.Wireless.Extensions.html
-fn get_signal_quality(interface: &str) -> Option<String> {
+fn signal_quality(interface: &str) -> Option<String> {
     let raw = fs::read_to_string("/proc/net/wireless").ok()?;
 
     for line in raw.lines() {
@@ -95,4 +95,31 @@ fn get_signal_quality(interface: &str) -> Option<String> {
     }
 
     None
+}
+
+/// Display the current volume level as reported by `amixer`
+pub fn amixer_volume(channel: &'static str, style: &TextStyle) -> RefreshText {
+    RefreshText::new(style, move || amixer_text(channel).unwrap_or_default())
+}
+
+// Parse the current volume as a percentage from amixer.
+//
+// Expected output format:
+//   $ amixer sget Master
+//     Simple mixer control 'Master',0
+//       Capabilities: pvolume pvolume-joined pswitch pswitch-joined
+//       Playback channels: Mono
+//       Limits: Playback 0 - 127
+//       Mono: Playback 0 [0%] [-63.50dB] [on]
+fn amixer_text(channel: &str) -> Option<String> {
+    let raw = spawn_for_output(format!("amixer sget {channel}")).ok()?;
+
+    let vol = raw
+        .lines()
+        .last()?
+        .split_whitespace()
+        .nth(3)?
+        .replace(|c| "[]%".contains(c), "");
+
+    Some(format!(" {vol}%"))
 }
