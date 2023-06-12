94bf59c02946f8f79c5efa26ab5132d152c8fe41
sminez <sminez@users.noreply.github.com>
2023-06-12 21:19:27 +0000

    deploy: 927c31b7d30d121bfabb94ca6dc1da5cfceb501a

--- rustdoc/penrose/extensions/hooks/manage/sidebar-items.js
+++ rustdoc/penrose/extensions/hooks/manage/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["DefaultTiled","FloatingCentered","FloatingFixed"]};
+window.SIDEBAR_ITEMS = {"struct":["DefaultTiled","FloatingCentered","FloatingFixed","FloatingRelative","SetWorkspace"]};
--- rustdoc/penrose/x/atom/sidebar-items.js
+++ rustdoc/penrose/x/atom/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["AUTO_FLOAT_WINDOW_TYPES","UNMANAGED_WINDOW_TYPES"],"enum":["Atom"],"struct":["AtomIter"]};
+window.SIDEBAR_ITEMS = {"constant":["AUTO_FLOAT_WINDOW_TYPES"],"enum":["Atom"],"struct":["AtomIter"]};
