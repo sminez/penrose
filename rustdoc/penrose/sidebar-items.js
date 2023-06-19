76f4edbdcef938ddb26c59af32b558a37a9899ca
sminez <sminez@users.noreply.github.com>
2023-06-19 06:43:18 +0000

    deploy: 49703388b62906b3836ebcf1924c12ed1861a6ba

--- rustdoc/penrose/sidebar-items.js
+++ rustdoc/penrose/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Error"],"macro":["custom_error","map","simple_transformer","stack"],"mod":["builtin","core","extensions","pure","util","x","x11rb"],"struct":["Color","Xid"],"type":["Result"]};
+window.SIDEBAR_ITEMS = {"enum":["Error"],"macro":["custom_error","manage_hooks","map","simple_transformer","stack"],"mod":["builtin","core","extensions","pure","util","x","x11rb"],"struct":["Color","Xid"],"type":["Result"]};
