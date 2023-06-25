9833616f4c64609419264a1c0a7ef7d1ae4c1c9b
sminez <sminez@users.noreply.github.com>
2023-06-25 16:47:06 +0000

    deploy: ecbf9cd1f5c2265ec7f808302f13273becb459d4

--- rustdoc/penrose_ui/bar/widgets/sidebar-items.js
+++ rustdoc/penrose_ui/bar/widgets/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["amixer_volume","battery_summary","current_date_and_time","wifi_network"],"mod":["debug"],"struct":["ActiveWindowName","CurrentLayout","RefreshText","RootWindowName","Text","Workspaces"],"trait":["Widget"]};
+window.SIDEBAR_ITEMS = {"fn":["amixer_volume","battery_summary","current_date_and_time","wifi_network"],"mod":["debug"],"struct":["ActiveWindowName","CurrentLayout","IntervalText","RefreshText","RootWindowName","Text","Workspaces"],"trait":["Widget"]};
