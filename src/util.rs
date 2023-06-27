7356541e43e68b531363dc83706f4a0c29ee3f4d
Innes Anderson-Morrison <innes.andersonmorrison@gmail.com>
2023-06-27 09:04:48 +0100

    adding helper function for printing and ASCII repr of a layout

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,9 @@
 //! Utility functions for use in other parts of penrose
-use crate::Result;
+use crate::{
+    core::layout::Layout,
+    pure::{geometry::Rect, Stack},
+    Result, Xid,
+};
 use std::{
     io::Read,
     process::{Command, Stdio},
@@ -106,4 +110,65 @@ pub fn spawn_for_output_with_args<S: Into<String>>(
 /// Use `notify-send` to display a message to the user
 pub fn notify(msg: &str) -> std::io::Result<()> {
     Command::new("notify-send").arg(msg).output().map(|_| ())
+}
+
+/// Run a given [`Layout`] for a stack of n clients and print a simple ASCII rendering
+/// of the resulting client positions.
+///
+/// This is provided as a helper function to make it easier to develop your own layout
+/// functions and see the results without needing to make use of them directly in your
+/// window manager. The output shown below is obtained by running the [MainAndStack][0]
+/// layout with a screen size of (40, 15) and 4 clients.
+///
+/// ### A note on screen dimensions
+/// In order to print out the ASCII representation of your layout, the "screen" dimensions
+/// being used will be significantly smaller than a typical real-life monitor. You may
+/// observe some unexpected behaviour from your layout when running this function if you
+/// have any hard coded values relating to screen size or individual client sizes.
+///
+/// ```text
+/// .........................................
+/// .                       .               .
+/// .                       .               .
+/// .                       .               .
+/// .                       .               .
+/// .                       .................
+/// .                       .               .
+/// .                       .               .
+/// .                       .               .
+/// .                       .               .
+/// .                       .................
+/// .                       .               .
+/// .                       .               .
+/// .                       .               .
+/// .                       .               .
+/// .........................................
+/// ```
+///
+/// [0]: crate::builtin::layout::MainAndStack
+pub fn print_layout_result<L: Layout>(
+    l: &mut L,
+    n_clients: u32,
+    screen_width: u32,
+    screen_height: u32,
+) {
+    let s: Stack<Xid> = Stack::try_from_iter((0..n_clients).map(Into::into)).expect("non-empty");
+    let (_, positions) = l.layout(&s, Rect::new(0, 0, screen_width, screen_height));
+
+    let mut screen = vec![vec![' '; (screen_width + 1) as usize]; (screen_height + 1) as usize];
+    for (_, Rect { x, y, w, h }) in positions.into_iter() {
+        for i in 0..=w {
+            screen[y as usize][(x + i) as usize] = '.';
+            screen[(y + h) as usize][(x + i) as usize] = '.';
+        }
+        for i in 0..=h {
+            screen[(y + i) as usize][x as usize] = '.';
+            screen[(y + i) as usize][(x + w) as usize] = '.';
+        }
+    }
+
+    for row in screen.into_iter() {
+        let chars: String = row.into_iter().collect();
+        println!("{chars}");
+    }
 }
