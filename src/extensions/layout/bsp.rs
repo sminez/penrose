356dc676267d9fce59040aa04eea4ce9f792747b
Innes Anderson-Morrison <innes.andersonmorrison@gmail.com>
2023-07-02 07:58:12 +0100

    This is initial work on a BSP layout
    Aiming for something like i3 in terms of functionality and making use
    of a BSP zipper in order to manipulate the tree. At this stage I've
    got a working tree zipper and node structs that support splitting,
    rotating and traversal, with the first pass at the layout algorithm
    itself just being a zip of the client stack with the Rect positions
    generated by the tree.
    
    There is still work to do around keeping the state of the tree in
    sync with the workspace (currently the tree is completely unaware of
    the workspace it is laying out) and I'll need to have a think about
    how this interacts with other layouts when switching between them.
    Initial thoughts are that I'll probably need to broadcast some messages
    to ALL layouts when clients are added and removed from workspaces
    as part of modify_and_refresh but we'll see when we get to that point.

--- src/extensions/layout/bsp.rs
+++ src/extensions/layout/bsp.rs
@@ -1,100 +1,320 @@
 //! A manual tiling layout based on binary space partitioning
+//!
+//! The data structure used for manipulating the tree and tracking focus is a 'Zipper'
+//! as described by Huet(97):
+//!   https://www.st.cs.uni-saarland.de/edu/seminare/2005/advanced-fp/docs/huet-zipper.pdf
+//!
+//! The Haskell Wiki has a more accessible article on how this concept works for binary
+//! trees (https://wiki.haskell.org/Zipper) as does Learn You A Haskell (http://learnyouahaskell.com/zippers)
 
-// TODO:
-// - Need a default way of filling the tree if we switch to this layout without
-//   a tree being present.
-//   - Auto-fill is probably easiest as the fibonacci layout?
-// - Need to be able to update state in an intuitive way when switching back to
-//   this layout from another one if the number of clients has changed.
-//   - Drop / extend from the leaves?
+// TODO: need a method on the Zipper to stash the Context for replaying so we can focus the root,
+//       iterate through the nodes and then move back to the focused point.
+//       - might just be simpler to clone and unravel?
+use crate::{
+    // builtin::layout::messages::{ExpandMain, ShrinkMain},
+    core::layout::{Layout, Message},
+    pure::{geometry::Rect, Stack},
+    Xid,
+};
 
-#[derive(Debug, Default, Clone)]
-enum DownNode {
-    #[default]
+use Context::*;
+use Node::*;
+use Side::*;
+
+/// An individual node in the BSP tree
+#[derive(Debug, Clone)]
+pub enum Node {
+    /// A leaf node that is occupied by a window
     Leaf,
+    /// A split with two child nodes
     Split {
+        /// The L/R ratio for the split
         ratio: f32,
+        /// Whether the split is horizontal or vertical
         hsplit: bool,
-        left: Box<DownNode>,
-        right: Box<DownNode>,
+        /// The left child
+        l: Box<Node>,
+        /// The right child
+        r: Box<Node>,
     },
 }
 
-impl DownNode {
-    fn split(&mut self, ratio: f32, hsplit: bool) {
+impl Node {
+    /// Split this node into two children
+    pub fn split(&mut self, ratio: f32, hsplit: bool) {
         *self = match self {
-            DownNode::Leaf => DownNode::Split {
+            Leaf => Split {
                 ratio,
                 hsplit,
-                left: Default::default(),
-                right: Default::default(),
+                l: Box::new(Leaf),
+                r: Box::new(Leaf),
             },
-
-            DownNode::Split { .. } => DownNode::Split {
+            Split { .. } => Split {
                 ratio,
                 hsplit,
-                left: Box::new(self.clone()),
-                right: Default::default(),
+                l: Box::new(self.clone()),
+                r: Box::new(Leaf),
             },
-        };
+        }
     }
-}
 
-#[derive(Debug, Clone)]
-struct Parent {
-    parent: Option<Box<Parent>>,
-    other: Box<Side>,
-    ratio: f32,
-    hsplit: bool,
-}
+    /// Rotate the orientation of a split node
+    pub fn rotate(&mut self) {
+        if let Split { hsplit, l, r, .. } = self {
+            *hsplit = !*hsplit;
+            l.rotate();
+            r.rotate();
+        }
+    }
 
-#[derive(Debug, Default, Clone)]
-enum UpNode {
-    #[default]
-    Leaf,
-    Parent(Parent),
+    fn take(&mut self) -> Self {
+        let mut n = Leaf;
+        std::mem::swap(self, &mut n);
+
+        n
+    }
+
+    fn into_rects(self, parent: Rect) -> Vec<Rect> {
+        match self {
+            Leaf => vec![parent],
+            Split {
+                ratio,
+                hsplit,
+                l,
+                r,
+            } => {
+                let (rl, rr) = if hsplit {
+                    parent.split_at_height_perc(ratio).unwrap()
+                } else {
+                    parent.split_at_width_perc(ratio).unwrap()
+                };
+
+                let mut rects = l.into_rects(rl);
+                rects.extend(r.into_rects(rr));
+
+                rects
+            }
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
 enum Side {
-    Left(UpNode),
-    Right(UpNode),
+    Left,
+    Right,
 }
 
-#[derive(Debug, Default, Clone)]
-pub struct BspZipper {
-    focus: DownNode,
-    parent: Option<Parent>,
+#[derive(Debug, Clone)]
+enum Context {
+    Root,
+    Branch {
+        ratio: f32,
+        hsplit: bool,
+        s: Side,
+        c: Box<Context>,
+        n: Node,
+    },
 }
 
-impl BspZipper {
-    fn focus_parent(&mut self) {
-        let mut p = match self.parent.take() {
-            Some(ref mut p) => p,
-            None => return,
-        };
+impl Context {
+    fn take(&mut self) -> Self {
+        let mut c = Root;
+        std::mem::swap(self, &mut c);
 
-        match p.side {
-            Side::Left(sibling) => {}
+        c
+    }
+}
 
-            Side::Right(sibling) => {}
+/// A BSP-tree zipper for traversing the current client tree and making modifications
+#[derive(Debug, Clone)]
+pub struct Zipper {
+    n: Node,
+    c: Context,
+}
+
+impl Zipper {
+    /// Create a new zipper from a given root node
+    pub fn from_root(n: Node) -> Zipper {
+        Zipper { n, c: Root }
+    }
+
+    /// Shift focus to the [Left] side of the focused [Split].
+    ///
+    /// If the focused [Node] is a [Leaf] then it is returned
+    /// unchanged.
+    pub fn focus_left(&mut self) {
+        let (n, c) = (self.n.take(), self.c.take());
+        (self.n, self.c) = match (n, c) {
+            (Leaf, c) => (Leaf, c),
+            (
+                Split {
+                    ratio,
+                    hsplit,
+                    l,
+                    r,
+                },
+                c,
+            ) => (
+                *l,
+                Branch {
+                    ratio,
+                    hsplit,
+                    s: Left,
+                    c: Box::new(c),
+                    n: *r,
+                },
+            ),
         }
     }
 
-    fn focus_left(&mut self) {}
+    /// Shift focus to the [Right] side of the focused [Split].
+    ///
+    /// If the focused [Node] is a [Leaf] then it is returned
+    /// unchanged.
+    pub fn focus_right(&mut self) {
+        let (n, c) = (self.n.take(), self.c.take());
+        (self.n, self.c) = match (n, c) {
+            (Leaf, c) => (Leaf, c),
+            (
+                Split {
+                    ratio,
+                    hsplit,
+                    l,
+                    r,
+                },
+                c,
+            ) => (
+                *r,
+                Branch {
+                    ratio,
+                    hsplit,
+                    s: Right,
+                    c: Box::new(c),
+                    n: *l,
+                },
+            ),
+        }
+    }
 
-    fn focus_right(&mut self) {}
+    /// Shift focus to the parent of the focused [Node].
+    ///
+    /// If the focused [Node] is already the [Root] of the tree then
+    /// it is returned unchanged.
+    pub fn focus_up(&mut self) {
+        let (n, c) = (self.n.take(), self.c.take());
+
+        (self.n, self.c) = match c {
+            Root => (n, Root),
+            Branch {
+                s,
+                c,
+                n: cn,
+                ratio,
+                hsplit,
+            } => {
+                let (l, r) = match s {
+                    Left => (Box::new(n), Box::new(cn)),
+                    Right => (Box::new(cn), Box::new(n)),
+                };
+                (
+                    Split {
+                        ratio,
+                        hsplit,
+                        l,
+                        r,
+                    },
+                    *c,
+                )
+            }
+        }
+    }
+
+    /// Focus the root of the tree
+    pub fn focus_root(&mut self) {
+        if matches!(self.c, Root) {
+            return;
+        }
+        self.focus_up();
+        self.focus_root();
+    }
+
+    /// Return a clone of the underlying BSP tree
+    pub fn clone_tree(&self) -> Node {
+        let mut z = self.clone();
+        z.focus_root();
+        z.n
+    }
+}
+
+/// A manual tiling layout using binary space partitioning.
+#[derive(Debug, Clone)]
+pub struct BSP {
+    zipper: Zipper,
+    hsplit: bool,
+}
+
+impl BSP {
+    /// Split the focused [Node] and then focus the [Right] side of the
+    /// new child [Split].
+    pub fn split(&mut self) {
+        self.zipper.n.split(0.5, self.hsplit);
+        self.zipper.focus_right();
+    }
+
+    /// Toggle the orientation of future splits between horizontal and vertical
+    pub fn toggle_orientation(&mut self) {
+        self.hsplit = !self.hsplit;
+    }
+}
+
+impl Default for BSP {
+    fn default() -> Self {
+        Self {
+            zipper: Zipper::from_root(Leaf),
+            hsplit: false,
+        }
+    }
+}
+
+impl Layout for BSP {
+    fn name(&self) -> String {
+        if self.hsplit { "BSP-" } else { "BSP|" }.to_string()
+    }
+
+    fn boxed_clone(&self) -> Box<dyn Layout> {
+        Box::new(self.clone())
+    }
+
+    fn layout(&mut self, s: &Stack<Xid>, r: Rect) -> (Option<Box<dyn Layout>>, Vec<(Xid, Rect)>) {
+        let rs = self.zipper.clone_tree().into_rects(r);
+        let positions = s.iter().zip(rs).map(|(&id, r)| (id, r)).collect();
+
+        (None, positions)
+    }
+
+    fn handle_message(&mut self, _: &Message) -> Option<Box<dyn Layout>> {
+        None
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::util::print_layout_result;
 
     #[test]
-    fn split_works() {
-        let mut z = BspZipper::default();
-        z.focus.split(0.4, true);
-        z.focus.split(0.7, false);
-        assert_eq!(format!("{z:?}"), "");
+    fn tree_zipper_traversal() {
+        let mut bsp = BSP::default();
+        bsp.split();
+        bsp.split();
+        bsp.toggle_orientation();
+        bsp.split();
+        bsp.zipper.focus_up();
+        bsp.zipper.focus_up();
+
+        print_layout_result(&mut bsp, 6, 40, 15);
+
+        bsp.zipper.n.rotate();
+        print_layout_result(&mut bsp, 6, 40, 15);
     }
 }
