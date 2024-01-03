7a867f625004fb753506267e96fb726a0b8032d9
sminez <sminez@users.noreply.github.com>
2024-01-03 10:44:57 +0000

    deploy: 7b7ed7568968f2ce914952eba7ad46b0d2d259bf

--- rustdoc/implementors/bitflags/traits/trait.PublicFlags.js
+++ rustdoc/implementors/bitflags/traits/trait.PublicFlags.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"penrose":[["impl PublicFlags for <a class=\"struct\" href=\"penrose/x/property/struct.WmHintsFlags.html\" title=\"struct penrose::x::property::WmHintsFlags\">WmHintsFlags</a>"],["impl PublicFlags for <a class=\"struct\" href=\"penrose/x/property/struct.WmNormalHintsFlags.html\" title=\"struct penrose::x::property::WmNormalHintsFlags\">WmNormalHintsFlags</a>"]]
+"penrose":[["impl PublicFlags for <a class=\"struct\" href=\"penrose/x/property/struct.WmNormalHintsFlags.html\" title=\"struct penrose::x::property::WmNormalHintsFlags\">WmNormalHintsFlags</a>"],["impl PublicFlags for <a class=\"struct\" href=\"penrose/x/property/struct.WmHintsFlags.html\" title=\"struct penrose::x::property::WmHintsFlags\">WmHintsFlags</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- rustdoc/implementors/core/ops/deref/trait.Deref.js
+++ rustdoc/implementors/core/ops/deref/trait.Deref.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"penrose":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"penrose/struct.Xid.html\" title=\"struct penrose::Xid\">Xid</a>"]]
+"penrose":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"penrose/struct.Xid.html\" title=\"struct penrose::Xid\">Xid</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
