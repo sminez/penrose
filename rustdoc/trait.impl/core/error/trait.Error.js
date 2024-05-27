997002941e6411baf2283424d893124f82238730
sminez <sminez@users.noreply.github.com>
2024-05-27 19:13:15 +0000

    deploy: b4b511c9ef396a7be248699580e75a09a8e0e149

--- rustdoc/trait.impl/core/error/trait.Error.js
+++ rustdoc/trait.impl/core/error/trait.Error.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"penrose":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"penrose/enum.Error.html\" title=\"enum penrose::Error\">Error</a>"]],
-"penrose_ui":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"penrose_ui/enum.Error.html\" title=\"enum penrose_ui::Error\">Error</a>"]]
+"penrose":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"penrose/enum.Error.html\" title=\"enum penrose::Error\">Error</a>"]],
+"penrose_ui":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"penrose_ui/enum.Error.html\" title=\"enum penrose_ui::Error\">Error</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
