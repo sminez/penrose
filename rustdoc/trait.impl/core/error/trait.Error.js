729225407adba01a8fbbd556a27c9a32ed52f1c6
sminez <sminez@users.noreply.github.com>
2025-04-15 19:38:07 +0000

    deploy: b03d7f931e895f63f8fabb00dfb765a5578bdf58

--- rustdoc/trait.impl/core/error/trait.Error.js
+++ rustdoc/trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["penrose",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"penrose/enum.Error.html\" title=\"enum penrose::Error\">Error</a>"]]],["penrose_ui",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"penrose_ui/enum.Error.html\" title=\"enum penrose_ui::Error\">Error</a>"]]]]);
+    var implementors = Object.fromEntries([["penrose",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"penrose/enum.Error.html\" title=\"enum penrose::Error\">Error</a>"]]],["penrose_ui",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"penrose_ui/enum.Error.html\" title=\"enum penrose_ui::Error\">Error</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
