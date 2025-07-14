bcb98da8df3b45f3288a1f399df33358e3ad07d5
sminez <sminez@users.noreply.github.com>
2025-07-14 08:57:26 +0000

    deploy: 7ff8cd9a24ee871ea141cc64886ef7f003a547ab

--- rustdoc/trait.impl/core/ops/deref/trait.Deref.js
+++ rustdoc/trait.impl/core/ops/deref/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["penrose",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"penrose/struct.Xid.html\" title=\"struct penrose::Xid\">Xid</a>"]]]]);
+    var implementors = Object.fromEntries([["penrose",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"penrose/struct.Xid.html\" title=\"struct penrose::Xid\">Xid</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
